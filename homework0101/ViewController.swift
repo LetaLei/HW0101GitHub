6afb8b5e0f0453208c79cb83db2e507486946a6e
max <max1218@gmail.com>
2020-07-10 19:54:58 +0800

    Initial Commit

---
+++ homework0101/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework0101
+//
+//  Created by max on 2020/7/10.
+//  Copyright © 2020 onetutu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
